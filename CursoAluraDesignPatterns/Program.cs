be05e3302042c4f8970f2791ef680d16fd1fb973
renatodvm <renatodvm@gmail.com>
2016-05-18 18:04:17 -0300

    Aula 1 - Parte 1

---
+++ CursoAluraDesignPatterns/Program.cs
@@ -0,0 +1,40 @@
+﻿using CursoAluraDesignPatterns.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoAluraDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imposto iss = new Iss();
+            Imposto icms = new Icms();
+            Imposto iccc = new Iccc();
+            Orcamento orcamento = new Orcamento(500.0);
+            CalculadorDeImposto calculador = new CalculadorDeImposto();
+
+            // Calculando o ISS
+            calculador.RealizaCalculo(orcamento, iss);
+
+            // Calculando o ICMS        
+            calculador.RealizaCalculo(orcamento, icms);
+
+            Console.Write("Iccc < 1000 (5%): ");
+            calculador.RealizaCalculo(orcamento, iccc);
+
+            Orcamento orcamento2 = new Orcamento(1001);
+            Console.Write("Iccc < 3000 (7%): ");
+            calculador.RealizaCalculo(orcamento2, iccc);
+
+            Orcamento orcamento3 = new Orcamento(3001);
+            Console.Write("Iccc > 3000 (8%): ");
+            calculador.RealizaCalculo(orcamento3, iccc);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CursoAluraDesignPatterns/Strategy/Iccc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoAluraDesignPatterns.Strategy
+{
+    public class Iccc : Imposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (orcamento.Valor < 1000)
+                return orcamento.Valor * .05;
+
+            if (orcamento.Valor < 3000)
+                return orcamento.Valor * .07;
+
+            return orcamento.Valor * .08 + 30;
+        }
+    }
+}
